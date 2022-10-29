aaa53b2a907478024d75be5df2706c097dc35f93
LDSamuelGuo <105694995+LDSamuelGuo@users.noreply.github.com>
2022-10-29 20:41:31 +1000

    Update hashtable.c

--- hashtable.c
+++ hashtable.c
@@ -1,192 +1,124 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h> 
+
+#define LEVELS 5
+#define CAPACITY 20
+#define MAX_CARPARK 100
+#define STORAGE_CAPACITY 7
+
+typedef struct vehicle {
+	char plate[STORAGE_CAPACITY];
+    clock_t entranceTime;
+    clock_t parkTime;
+    int level;
+    bool exitStatus;
+    int LPRcount;
+} vehicle_t;
+
+typedef struct htable {
+	int size;
+	vehicle_t vehicle[MAX_CARPARK];
+} htable_t;
 
 
-#include <inttypes.h> // for portable integer declarations
-#include <stdbool.h>  // for bool type
-#include <stdio.h>    // for printf()
-#include <stdlib.h>   // for malloc(), free(), NULL
-#include <string.h>   // for strcmp()
-
-// An item inserted into a hash table.
-typedef struct item item_t;
-
-
-struct item
-{
-    char *key;
-    int value;
-    item_t *next;
-};
-
-void item_print(item_t *i)
-{
-    printf("key=%s value=%d", i->key, i->value);
-}
-
-// A hash table mapping a string to an integer.
-typedef struct htab htab_t;
-struct htab
-{
-    item_t **buckets;
-    size_t size;
-};
-
-// Initialise a new hash table with n buckets.
-bool htab_init(htab_t *h, size_t n)
-{
-    // TODO: implement this function
-    h->buckets = (item_t **)calloc(n,sizeof(item_t *));
-    h->size = n; 
-
-    return h->buckets != NULL;    
-}
-
-// The Bernstein hash function.
-
-size_t djb_hash(char *s)
-{
-    size_t hash = 5381;
-    int c;
-    while ((c = *s++) != '\0')
+// Initialise the storage
+bool htable_init(htable_t *htable){
+    htable->size = 0;
+    int i = 0;
+    while (i < MAX_CARPARK)
     {
-        // hash = hash * 33 + c
-        hash = ((hash << 5) + hash) + c;
+        strcpy(htable->vehicle[i].plate, "empty");
+        htable->vehicle[i].entranceTime = 0;
+        htable->vehicle[i].parkTime = 0;
+        htable->vehicle[i].exitStatus = false;
+        htable->vehicle[i].level = 1;
+        htable->vehicle[i].LPRcount = 0;
+        i++;
     }
-    return hash;
 }
 
-// Calculate the offset for the bucket for key in hash table.
-size_t htab_index(htab_t *h, char *key)
-{
-    return djb_hash(key) % h->size;
-}
-
-// Find pointer to head of list for key in hash table.
-item_t *htab_bucket(htab_t *h, char *key)
-{
-    // TODO: implement this function (uses htab_index())
-    return h->buckets[htab_index(h, key)];
-}
-
-// Find an item for key in hash table.
-
-item_t *htab_find(htab_t *h, char *key)
-{
-    // TODO: implement this function
-    item_t *current = htab_bucket(h, key);
-
-    for (; current != NULL; current = current->next){
-        if (strcmp(current->key,key) == 0){
-            return current;
+// Find index of plate (from numberplate)
+int findIndex(htable_t* htable, char *plate){
+    int index, i=0 ;
+    while (i < htable->size){
+        if (memcmp(htable->vehicle[i].plate, plate,7) == 0){
+            index = i;
+            return index;
         }
+        i++;
     }
-
-    return NULL;
+    return 1;
 }
 
-// Add a key with value to the hash table.
-
-bool htab_add(htab_t *h, char *key, int value)
-{
- 
-    item_t *new_node = (item_t *)malloc(sizeof(item_t));
-    if (new_node == NULL){
-        return false;
-    }
-
-    size_t bucket_idx = htab_index(h,key);
-
-    new_node->key = key;
-    new_node->value = value;
-    new_node->next = h->buckets[bucket_idx];
-    h->buckets[bucket_idx] = new_node;
-
-    return true;
-}
-
-// Print the hash table.
-
-void htab_print(htab_t *h)
-{
-    printf("hash table with %d buckets\n", h->size);
-    for (size_t i = 0; i < h->size; ++i)
-    {
-        printf("bucket %d: ", i);
-        if (h->buckets[i] == NULL)
-        {
-            printf("empty\n");
-        }
-        else
-        {
-            for (item_t *j = h->buckets[i]; j != NULL; j = j->next)
-            {
-                item_print(j);
-                if (j->next != NULL)
-                {
-                    printf(" -> ");
-                }
-            }
-            printf("\n");
-        }
+// Print vehicles 
+void printVehicleList(htable_t* htable){
+    printf("NUMBER OF PLATES ARE: %d\n", htable->size); 
+    int i = 0;
+    for (i < htable->size){
+        printf("Plate number %d is: %s\n", i, htable->vehicle[i].plate);
+        printf("Entrance time of vehicle %d is: %ld\n", i, htable->vehicle[i].entranceTime);
+        printf("Park time of vehicle %d is: %ld\n", i, htable->vehicle[i].parkTime);
+        printf("Level of vehicle %d is: %d\n", i, htable->vehicle[i].level);
+        printf("Number of LRP count %d is: %d\n", i, htable->vehicle[i].LPRcount);
+        i++;
     }
 }
 
-// Delete an item with key from the hash table.
 
-void htab_delete(htab_t *h, char *key)
-{
-    item_t *head = htab_bucket(h, key);
-    item_t *current = head;
-    item_t *previous = NULL;
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            if (previous == NULL)
-            { // first item in list
-                h->buckets[htab_index(h, key)] = current->next;
-            }
-            else
-            {
-                previous->next = current->next;
-            }
-            free(current);
+
+// Add a vehicle to the storage
+bool htable_add(htable_t *htable, char *plate, clock_t entranceTime, clock_t parkTime, int level){
+    int old_size = htable->size;
+    memcpy(htable->vehicle[old_size].plate, plate, 7);
+    htable->vehicle[old_size].entranceTime = entranceTime;
+    htable->vehicle[old_size].parkTime = parkTime;
+    htable->vehicle[old_size].level = level;
+    htable->vehicle[old_size].exitStatus = false;
+    htable->vehicle[old_size].LPRcount = 0;
+    htable->size = old_size + 1;
+}
+
+// Remove a vehicle based on its plate
+void htable_delete(htable_t* htable, char *plate){
+    int old_size = htable->size;
+    vehicle_t old_vehicle[MAX_CARPARK];
+    int base;
+
+    for (int i = 0; i < old_size; i++){
+        memcpy(old_vehicle[i].plate, htable->vehicle[i].plate, 7);
+        old_vehicle[i].entranceTime = htable->vehicle[i].entranceTime;
+        old_vehicle[i].parkTime = htable->vehicle[i].parkTime;
+        old_vehicle[i].level = htable->vehicle[i].level;
+        old_vehicle[i].exitStatus = htable->vehicle[i].exitStatus;
+        old_vehicle[i].LPRcount = htable->vehicle[i].LPRcount;
+    }
+
+    for (int i = 0; i < old_size; i++){
+        if (memcmp(htable->vehicle[i].plate, plate, 7) == 0){
+            loc = i;
             break;
         }
-        previous = current;
-        current = current->next;
     }
+
+    for (int i = 0; i < loc; i++){
+        memcpy(htable->vehicle[i].plate, old_vehicle[i].plate,7);
+        htable->vehicle[i].entranceTime = old_vehicle[i].entranceTime;
+        htable->vehicle[i].parkTime = old_vehicle[i].parkTime;
+        htable->vehicle[i].level = old_vehicle[i].level;
+        htable->vehicle[i].exitStatus = old_vehicle[i].exitStatus;
+        htable->vehicle[i].LPRcount = old_vehicle[i].LPRcount;
+    }
+
+    for (int i = base; i < old_size - 1; i++){
+        memcpy(htable->vehicle[i].plate, old_vehicle[i + 1].plate,7);
+        htable->vehicle[i].entranceTime = old_vehicle[i + 1].entranceTime;
+        htable->vehicle[i].parkTime = old_vehicle[i + 1].parkTime;
+        htable->vehicle[i].level = old_vehicle[i + 1].level;
+        htable->vehicle[i].exitStatus = old_vehicle[i + 1].exitStatus;
+        htable->vehicle[i].LPRcount = old_vehicle[i + 1].LPRcount;
+    }
+
+    htable->size = old_size - 1;
 }
-
-// Destroy an initialised hash table.
-
-void htab_destroy(htab_t *h)
-{
-   
-    for (int i = 0; i < h->size; i++){
-        item_t *current = h->buckets[i];
-
-        while (current != NULL){
-            item_t *next = current->next;
-            free(current);
-
-            current = next;
-        }
-    }
-
-    free(h->buckets);
-    h->buckets= NULL;
-    h->size = 0;
-}
-
-int main(int argc, char **argv)
-{
-    // create a hash table with 10 buckets
-    printf("creating hash table:\n");
-    size_t buckets = 10;
-    htab_t h;
-    if (!htab_init(&h, buckets))
-    {
-        printf("failed to initialise hash table\n");
-        return EXIT_FAILURE;
-    }
-
